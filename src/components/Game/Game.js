112d09f40a01e9b64c70dac6d7ead18ef663b1c1
Hadley Wickham <h.wickham@gmail.com>
2025-01-04 09:07:02 -0600

    More refactoring

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,20 +5,21 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
+import { range } from "../../utils";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-const initGuesses = Array.from({ length: NUM_OF_GUESSES_ALLOWED }, (i, _) => ({
-  value: "",
-  id: crypto.randomUUID(),
-}));
-
 function Game() {
   const [curGuess, setCurGuess] = React.useState(0);
-  const [guesses, setGuesses] = React.useState(initGuesses);
+  const [guesses, setGuesses] = React.useState(
+    range(0, NUM_OF_GUESSES_ALLOWED).map(() => ({
+      value: "     ",
+      id: crypto.randomUUID(),
+    }))
+  );
 
   function addGuess(guess) {
     console.log({ guess });
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { range } from "../../utils";
 
 function Guess({ value }) {
-  const letters = value === "" ? range(0, 5).map(() => "") : value.split("");
-
   return (
     <p className="guess">
-      {letters.map((letter, index) => (
+      {value.split("").map((letter, index) => (
         <span className="cell" key={index}>
           {letter}
         </span>
